54e35a4c4d931be92cf3b644c575daff32021e06
Daniel Martin <danielwmartin1@gmail.com>
2025-03-11 04:44:10 -0400

    feat: refactor Todo fetching and editing logic using axios for improved API integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-// ...existing code...
-fetch('http://localhost:5001/todos')
-  .then(response => response.json())
-  .then(data => {
-    // ...existing code...
-  })
-  .catch(error => {
-    console.error('Error fetching todos:', error);
-  });
-// ...existing code...
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TodoItem from './components/TodoItem';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const res = await axios.get('/todos');
+      setTodos(res.data);
+    };
+    fetchTodos();
+  }, []);
+
+  const editTodo = async (id, task) => {
+    const res = await axios.put(`/editTodo/${id}`, { task });
+    setTodos(todos.map(todo => (todo._id === id ? res.data : todo)));
+  };
+
+  const completeTodo = async (id) => {
+    const res = await axios.put(`/completeTodo/${id}`);
+    setTodos(todos.map(todo => (todo._id === id ? res.data : todo)));
+  };
+
+  return (
+    <div>
+      {/* ...existing code... */}
+      {todos.map(todo => (
+        <TodoItem
+          key={todo._id}
+          todo={todo}
+          onEdit={editTodo}
+          onComplete={completeTodo}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
