33ef9b001ec62573b50a718241fa7b9cda75c7cb
Daniel Martin <danielwmartin1@gmail.com>
2025-03-16 22:26:01 -0400

    refactor: remove Todo components and consolidate app structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TodoList from './components/TodoList';
-import Filter from './components/Filter';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
