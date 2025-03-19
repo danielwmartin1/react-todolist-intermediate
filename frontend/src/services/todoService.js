03f62e2d9d9ddf67c5bc1e80586126799307739a
Daniel Martin <danielwmartin1@gmail.com>
2025-03-19 00:13:27 -0400

    chore: add concurrently package and update backend URL in todoService

--- frontend/src/services/todoService.js
+++ frontend/src/services/todoService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const backendUrl = 'http://localhost:5001/api'; // Ensure the base URL is correct
+const backendUrl = 'https://dwm-intermediate-react-todolist-api.vercel.app/api/todos'; // Ensure the base URL is correct
 
 export const fetchTodos = async () => {
   try {
-    return await axios.get(`${backendUrl}/todos`);
+    return await axios.get(`${backendUrl}`);
   } catch (error) {
     console.error('Error fetching todos:', error);
     throw error;
@@ -13,7 +13,7 @@ export const fetchTodos = async () => {
 
 export const addTodo = async (newTodoText) => {
   try {
-    return await axios.post(`${backendUrl}/todos`, { text: newTodoText });
+    return await axios.post(`${backendUrl}`, { text: newTodoText });
   } catch (error) {
     console.error('Error adding todo:', error);
     throw error;
@@ -22,7 +22,7 @@ export const addTodo = async (newTodoText) => {
 
 export const toggleCompletion = async (id, completed) => {
   try {
-    return await axios.patch(`${backendUrl}/todos/${id}`, { completed });
+    return await axios.patch(`${backendUrl}/${id}`, { completed });
   } catch (error) {
     console.error('Error toggling completion:', error);
     throw error;
@@ -31,7 +31,7 @@ export const toggleCompletion = async (id, completed) => {
 
 export const deleteTodo = async (id) => {
   try {
-    return await axios.delete(`${backendUrl}/todos/${id}`);
+    return await axios.delete(`${backendUrl}/${id}`);
   } catch (error) {
     console.error('Error deleting todo:', error);
     throw error;
@@ -40,7 +40,7 @@ export const deleteTodo = async (id) => {
 
 export const saveEdit = async (id, editText, completed) => {
   try {
-    return await axios.put(`${backendUrl}/todos/${id}`, { text: editText, completed });
+    return await axios.put(`${backendUrl}/${id}`, { text: editText, completed });
   } catch (error) {
     console.error('Error saving edit:', error);
     throw error;
